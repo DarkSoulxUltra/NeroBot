f552b8e84b007b1da706686005878853904ed342
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-29 18:41:19 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Flare Robot
-:: Enables virtual env mode and then starts Flare_Robot
-env\scripts\activate.bat && py -m Flare-Robot
+TITLE Nero Robot
+:: Enables virtual env mode and then starts NeroBot
+env\scripts\activate.bat && py -m NeroBot
